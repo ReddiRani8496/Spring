9ce53722ad775305c6ffffc627925420b132b516
reddirani8496 <reddiranibattuluri@gmail.com>
2024-07-01 08:39:33 +0530

    servlet program uploaded..

---
+++ SevletEx/src/main/java/com/xable/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.xable;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Do get method from servlets");
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h2>Hello from servlet!</h2>");
+    }
+}
